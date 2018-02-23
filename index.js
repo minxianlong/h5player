fc1f629f8be272031ce3ed4fbd5b4a7674b0711f
Xianlong Min <66794531@qq.com>
2018-02-23 10:51:19 -0400

    initial codes

---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (request, response) {
+    var pathName = url.parse(request.url).pathname;
+    console.log(pathName);
+    fs.readFile(__dirname + pathName, function (err, data) {
+        if (err) {
+            response.writeHead(404, {'Content-type': 'text/plan'});
+            response.write('Page Was Not Found');
+            response.end();
+        } else {
+            response.writeHead(200);
+            response.write(data);
+            response.end();
+        }
+    })
+}).listen(7001);
---
+++ js/controller/MainController.js
@@ -0,0 +1,115 @@
+angular.module("myApp", [])
+    .controller('MainController', function ($scope, $timeout) {
+        function timeUpdate() {
+            var index = this.playlist.currentIndex();
+            var currentTime = this.currentTime();
+
+            this.playData.width = (currentTime + index * 10) * 100 / (this.playData.playlist.length * 10) + '%';
+        }
+
+        $scope.refresh = function (player) {
+            console.log($scope.videoPlayers[player.id]);
+            if (player) {
+                if (player.visible) {
+                    $scope.videoPlayers[player.id].play();
+                }
+                else {
+                    $scope.videoPlayers[player.id].pause();
+                }
+
+            }
+        };
+
+        $scope.load = function () {
+            for (var i = 0; i < $scope.playerData.length; i++) {
+                var id = $scope.playerData[i].id;
+                var player = videojs(id, {
+                    controls: false,
+                    autoplay: false,
+                    loop: false,
+                    preload: 'auto'
+                });
+
+                player.playData = $scope.playerData[i];
+                player.playlist($scope.playerData[i].playlist);
+                player.playlist.autoadvance(0);
+                player.on("timeupdate", timeUpdate);
+                $scope.videoPlayers[$scope.playerData[i].id] = player;
+
+                if ($scope.playerData[i].visible) {
+                    player.play();
+                }
+
+            }
+        };
+
+        $scope.init = function () {
+            $scope.playerData = [
+                {
+                    id: 'video_1',
+                    visible: true,
+                    playlist: [{
+                        sources: [{
+                            src: 'res/1.mp4',
+                            type: 'video/mp4'
+                        }]
+                    }, {
+                        sources: [{
+                            src: 'res/2.mp4',
+                            type: 'video/mp4'
+                        }]
+                    }, {
+                        sources: [{
+                            src: 'res/3.mp4',
+                            type: 'video/mp4'
+                        }]
+                    }],
+                    width: '0%'
+                },
+                {
+                    id: 'video_2',
+                    visible: true,
+                    playlist: [{
+                        sources: [{
+                            src: 'res/3.mp4',
+                            type: 'video/mp4'
+                        }]
+                    }, {
+                        sources: [{
+                            src: 'res/4.mp4',
+                            type: 'video/mp4'
+                        }]
+                    }],
+                    width: '0%'
+                },
+                {
+                    id: 'video_3',
+                    visible: true,
+                    playlist: [{
+                        sources: [{
+                            src: 'res/4.mp4',
+                            type: 'video/mp4'
+                        }]
+                    }, {
+                        sources: [{
+                            src: 'res/5.mp4',
+                            type: 'video/mp4'
+                        }]
+                    }],
+                    width: '0%'
+                }
+            ];
+
+            $scope.videoPlayers = {};
+
+            $scope.value = {
+                width: '0%'
+            };
+        };
+
+        $scope.init();
+
+        $timeout(function () {
+            $scope.load();
+        }, 1000);
+    });
